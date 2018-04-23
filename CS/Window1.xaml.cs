6ea096078a68b945a09bec8bdcabcedc57163372
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using DevExpress.Xpf.Charts;
+
+namespace DXChartsTooltips {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+
+        private void chartControl1_MouseMove(object sender, MouseEventArgs e) {
+            ChartHitInfo hitInfo = chartControl1.CalcHitInfo(e.GetPosition(chartControl1));
+            
+            if (hitInfo != null && hitInfo.SeriesPoint != null) {
+                SeriesPoint point = hitInfo.SeriesPoint;
+
+                tooltip_text.Text = string.Format("Series = {0}\nArgument = {1}\nValue = {2}",
+                    point.Series.DisplayName, point.Argument, point.Value);
+                tooltip1.Placement = PlacementMode.Mouse;
+                
+                tooltip1.IsOpen = true;
+                Cursor = Cursors.Hand;
+            }
+            else {
+                tooltip1.IsOpen = false;
+                Cursor = Cursors.Arrow;
+            }
+        }
+
+        private void chartControl1_MouseLeave(object sender, MouseEventArgs e) {
+            tooltip1.IsOpen = false;
+        }
+
+    }
+}
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,38 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls.Primitives
+Imports System.Windows.Input
+Imports DevExpress.Xpf.Charts
+
+Namespace DXChartsTooltips
+
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub chartControl1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
+			Dim hitInfo As ChartHitInfo = chartControl1.CalcHitInfo(e.GetPosition(chartControl1))
+
+			If hitInfo IsNot Nothing AndAlso hitInfo.SeriesPoint IsNot Nothing Then
+				Dim point As SeriesPoint = hitInfo.SeriesPoint
+
+				tooltip_text.Text = String.Format("Series = {0}" & Constants.vbLf & "Argument = {1}" & _
+					Constants.vbLf & "Value = {2}", point.Series.DisplayName, point.Argument, point.Value)
+				tooltip1.Placement = PlacementMode.Mouse
+
+				tooltip1.IsOpen = True
+				Cursor = Cursors.Hand
+			Else
+				tooltip1.IsOpen = False
+				Cursor = Cursors.Arrow
+			End If
+		End Sub
+
+		Private Sub chartControl1_MouseLeave(ByVal sender As Object, ByVal e As MouseEventArgs)
+			tooltip1.IsOpen = False
+		End Sub
+
+	End Class
+End Namespace
