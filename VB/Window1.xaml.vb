87dbf736af461955c85b73189e32825910c98730
DevExpressExampleBot <fakeEmail>
2025-03-07 14:17:09 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,4 +1,3 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 Imports System.Windows.Controls.Primitives
 Imports System.Windows.Input
@@ -6,33 +5,29 @@ Imports DevExpress.Xpf.Charts
 
 Namespace DXChartsTooltips
 
-	Partial Public Class Window1
-		Inherits Window
+    Public Partial Class Window1
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub chartControl1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
-			Dim hitInfo As ChartHitInfo = chartControl1.CalcHitInfo(e.GetPosition(chartControl1))
+        Private Sub chartControl1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
+            Dim hitInfo As ChartHitInfo = Me.chartControl1.CalcHitInfo(e.GetPosition(Me.chartControl1))
+            If hitInfo IsNot Nothing AndAlso hitInfo.SeriesPoint IsNot Nothing Then
+                Dim point As SeriesPoint = hitInfo.SeriesPoint
+                Me.tooltip_text.Text = String.Format("Series = {0}" & Microsoft.VisualBasic.Constants.vbLf & "Argument = {1}" & Microsoft.VisualBasic.Constants.vbLf & "Value = {2}", point.Series.DisplayName, point.Argument, point.Value)
+                Me.tooltip1.Placement = PlacementMode.Mouse
+                Me.tooltip1.IsOpen = True
+                Cursor = Cursors.Hand
+            Else
+                Me.tooltip1.IsOpen = False
+                Cursor = Cursors.Arrow
+            End If
+        End Sub
 
-			If hitInfo IsNot Nothing AndAlso hitInfo.SeriesPoint IsNot Nothing Then
-				Dim point As SeriesPoint = hitInfo.SeriesPoint
-
-				tooltip_text.Text = String.Format("Series = {0}" & vbLf & "Argument = {1}" & vbLf & "Value = {2}", point.Series.DisplayName, point.Argument, point.Value)
-				tooltip1.Placement = PlacementMode.Mouse
-
-				tooltip1.IsOpen = True
-				Cursor = Cursors.Hand
-			Else
-				tooltip1.IsOpen = False
-				Cursor = Cursors.Arrow
-			End If
-		End Sub
-
-		Private Sub chartControl1_MouseLeave(ByVal sender As Object, ByVal e As MouseEventArgs)
-			tooltip1.IsOpen = False
-		End Sub
-
-	End Class
+        Private Sub chartControl1_MouseLeave(ByVal sender As Object, ByVal e As MouseEventArgs)
+            Me.tooltip1.IsOpen = False
+        End Sub
+    End Class
 End Namespace
